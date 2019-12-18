b74c9fe48739d5755c46727f75554df917752665
Ilko Hoffmann <ilkohoffmann@ilko-dock.hausnetz.schellingstrasse.net>
2019-12-18 13:59:59 +0100

    remove context unit test

--- src/test/java/org/cofomo/authority/CofomoAuthorizationLibraryApplicationTests.java
+++ src/test/java/org/cofomo/authority/CofomoAuthorizationLibraryApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CofomoAuthorizationLibraryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
